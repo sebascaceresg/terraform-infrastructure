cfafd9ede1c73c15e80adab8c27dfff60fd5d656
Sebastian Cáceresg G. <streamly.tech@gmail.com>
2025-04-09 11:32:27 -0500

    FEAT: Create circleci config and hub-project infrastructure

---
+++ gcp-projects/hub-project/main.tf
@@ -0,0 +1,20 @@
+# <---------- VPC ----------> #
+module "vpc-private" {
+  source = "git@github.com:sebascaceresg/terraform-modules.git//GCP/vpc?ref=master"
+
+  # VPC #
+  region                  = var.region
+  project_id              = var.project_id
+  vpc_name                = "tf-private-vpc"
+  vpc_description         = "Private VPC for hub-project"
+  auto_create_subnetworks = false
+  routing_mode            = "REGIONAL"
+
+  # PSA/PSC #
+  create_private_service_access = true
+  ip_address                    = "10.240.0.0"
+  prefix_length                 = 24
+  ip_address_type               = "INTERNAL"
+  psa_purpose                   = "VPC_PEERING"
+
+}
--- gcp-projects/my-projects/data_sources.tf
+++
@@ -1,5 +0,0 @@
-data "google_service_account_access_token" "default" {
-  target_service_account = "terraform-service@my-projects-450518.iam.gserviceaccount.com"
-  scopes                 = ["userinfo-email", "cloud-platform"]
-  lifetime               = "300s"
-}
